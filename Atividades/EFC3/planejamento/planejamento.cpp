d4320a932ad209bad968f30fe3612756dd78bead
rafael_.moura <rafael.moura.dev.29@gmail.com>
2023-02-26 18:03:42 -0300

    🚀

--- Atividades/EFC3/planejamento/planejamento.cpp
+++ Atividades/EFC3/planejamento/planejamento.cpp
@@ -1,50 +1,72 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int min( int a, int b ) {
-  return  a < b ? a : b;
+
+bool DEBUG = false;
+
+void log( string x, bool should_log = true, bool verbose = DEBUG ) {
+  if ( verbose && should_log ) cout << x;
 }
 
-int funcao( int horas, int soma_das_rotas ) {
-  if ( soma_das_rotas > horas ) {
-    return soma_das_rotas - horas;
-  }
-  else {
-    return horas - soma_das_rotas;
-  }
+int funcao( int soma_das_rotas, int horas ) {
+  return soma_das_rotas < horas ? horas - soma_das_rotas : soma_das_rotas - horas;
 }
 
-int calcula( int matutinas[ ], int vespertinas[ ], int motoristas, int horas, int valor ) {
-  int matrix[motoristas][motoristas];
-  int solucao[motoristas];
+bool retornaMelhor( int old, int actual, int horas ) {
+  int f_old = funcao( old, horas );
+  int f_actual = funcao( actual, horas );
 
-  for ( int matutina = 0; matutina < motoristas; matutina++ ) {
-    for ( int vespertina = 0; vespertina < motoristas; vespertina++ ) {
-      matrix[matutina][vespertina] = matutinas[matutina] + vespertinas[vespertina];
-      cout << "\t" << matrix[matutina][vespertina];
-    }
-    cout << endl;
-  }
+  return f_old < f_actual ? false : true;
 }
+
 
 
 int planejamento( int motoristas, int horas, int valor ) {
-  int matutinas[motoristas];
-  int vespertinas[motoristas];
+  vector<int> matutinas( motoristas );
+  vector<int> vespertinas( motoristas );
 
   for ( int i = 0; i < motoristas; i++ ) {
     cin >> matutinas[i] >> vespertinas[i];
   }
 
-  return calcula( matutinas, vespertinas, motoristas, horas, valor );
+
+  int solucao[motoristas];
+
+  for ( int m = 0; m < motoristas; m++ ) {
+    int melhor = INT32_MAX;
+    int index_melhor = -1;
+    for ( int v = 0; v < ( int )vespertinas.size(); v++ ) {
+      int soma_das_rotas = matutinas[m] + vespertinas[v];
+
+      log( "\t" );
+      log( to_string( matutinas[m] + vespertinas[v] ), true );
+      log( to_string( matutinas[m] ), false );
+      log( to_string( vespertinas[v] ), false );
+      if ( retornaMelhor( melhor, soma_das_rotas, horas ) ) {
+        melhor = soma_das_rotas;
+        index_melhor = v;
+      }
+    }
+    if ( index_melhor >= 0 ) {
+      vespertinas.erase( vespertinas.begin() + index_melhor );
+    }
+    log( "\t" + to_string( vespertinas.size() ) + "\n" );
+    solucao[m] = melhor > horas ? ( melhor - horas ) * valor : 0;
+    if ( m > 0 )
+      solucao[m] += solucao[m - 1];
+  }
+
+
+  int resposta = solucao[motoristas - 1];
+  return resposta;
 }
 
 int main() {
   int motoristas, horas, valor;
 
   cin >> motoristas >> horas >> valor;
-
   while ( motoristas && horas && valor ) {
     cout << planejamento( motoristas, horas, valor );
     cin >> motoristas >> horas >> valor;
--- Atividades/EFC3/planejamento/x.py
+++ Atividades/EFC3/planejamento/x.py
@@ -19,24 +19,40 @@ def write(file, data, retry=2):
         else:
             os.system(f"touch {file}")
             if retry > 0:
-                write(data, file, retry - 1)
+                write(file, data, retry - 1)
             else:
                 print("Arquivo não existe!")
     except Exception as e:
         print(f"Não foi possível escrever!\nArquivo: {file}")
 
 
-x = read("test/1.in").split("JACARE")
+x = read("test/1.in").split("\nJACARE\n")
 o = read("test/1.out").split("\n")
 
 file = 3
 i = 0
+
+menor = {"file": 3, "motorista": 99, "horas": 99999}
+
 for y in x:
-    write(f"test/{file}.in", y)
+    entrada = y.split("\n")[0]
+    [motorista, horas, valor] = entrada.split(" ")
+
+    if (
+        menor["motorista"] >= int(motorista)
+        and menor["horas"] >= int(horas)
+        and motorista != "0"
+    ):
+        menor["motorista"] = int(motorista)
+        menor["horas"] = int(horas)
+        menor["file"] = file
+        print(menor)
+    write(f"test/{file}.in", f"{y}\n0 0 0")
     write(f"test/{file}.out", o[i])
     file += 1
     i += 1
-    if file % 2:
-        r = input(f"{file}: Ok?")
-        if r == "n" or r == "N":
-            exit(0)
+    # if not file % 10:
+    #     r = input(f"{file}: Ok?")
+    #     if r == "n" or r == "N":
+    #         exit(0)
+print(f"\nMenor {menor}")
